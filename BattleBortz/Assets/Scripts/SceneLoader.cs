49afc295b6768e09056dcee6deaed8451aa518e7
Cosima Peter <cs.manughian@gmail.com>
2019-03-29 15:52:36 -0700

    Basic splash screen

---
+++ BattleBortz/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField] int sceneNumber;
+
+    public void LoadScene(int sceneNumber)
+    {
+        SceneManager.LoadScene(sceneNumber);
+    }
+}
