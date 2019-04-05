33bc16cccdf91b19e567e39ed5ee31218b3473a8
Aaron Gutierrez <argutierrez92@gmail.com>
2019-04-05 11:16:00 -0700

    Pair programming - Bot movement and prefab updates

---
+++ BattleBortz/Assets/Scripts/BotMover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotMover : MonoBehaviour
+{
+    [SerializeField] float speed = 50f;
+    [SerializeField] float rotationSpeed = 150f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 velocity = Vector3.forward * speed;
+        Vector3 translation = Input.GetAxis("Vertical") * velocity;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+
+        transform.Translate(translation);
+        transform.Rotate(0, rotation, 0);
+    }
+}
