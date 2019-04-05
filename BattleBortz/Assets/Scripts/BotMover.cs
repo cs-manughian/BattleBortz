d3e02f52759b5acb796dc9f9723f074ce8fa45a1
Aaron Gutierrez <argutierrez92@gmail.com>
2019-04-05 16:56:48 -0700

    Pair programming - Applied forces for bot movement

--- BattleBortz/Assets/Scripts/BotMover.cs
+++ BattleBortz/Assets/Scripts/BotMover.cs
@@ -4,20 +4,33 @@ using UnityEngine;
 
 public class BotMover : MonoBehaviour
 {
-    [SerializeField] float speed = 50f;
-    [SerializeField] float rotationSpeed = 150f;
+    [SerializeField] float speed = 10000f;
+    [SerializeField] float rotationSpeed = 500f;
+    [SerializeField] int player;
+
+    Rigidbody _rigidbody;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        //_rigidbody.maxAngularVelocity = Mathf.Infinity;
+    }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        float horizontalAxis = player == 1 ? Input.GetAxis("Horizontal_P1") : Input.GetAxis("Horizontal_P2");
+        float verticalAxis = player == 1 ? Input.GetAxis("Vertical_P1") : Input.GetAxis("Vertical_P2");
+
         Vector3 velocity = Vector3.forward * speed;
-        Vector3 translation = Input.GetAxis("Vertical") * velocity;
-        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        Vector3 translation = verticalAxis * velocity;
+        Vector3 rotation = horizontalAxis * rotationSpeed * Vector3.up;
 
-        translation *= Time.deltaTime;
-        rotation *= Time.deltaTime;
+        translation *= Time.fixedDeltaTime;
+        rotation *= Time.fixedDeltaTime;
 
-        transform.Translate(translation);
-        transform.Rotate(0, rotation, 0);
+        Debug.Log(translation);
+        _rigidbody.AddRelativeForce(translation, ForceMode.Force);
+        _rigidbody.AddRelativeTorque(rotation, ForceMode.Acceleration);
     }
 }
