589fb4e8d1d76f3f1285453d524f665f38711ad1
Cosima Peter <cs.manughian@gmail.com>
2019-05-04 19:55:06 -0700

    Bear throwable prefab explodes - TODO: Refactor play hit fx from inherited class

--- BattleBortz/Assets/Scripts/Weapons/ThrowableWeapon.cs
+++ BattleBortz/Assets/Scripts/Weapons/ThrowableWeapon.cs
@@ -2,12 +2,16 @@
 
 public class ThrowableWeapon : MonoBehaviour
 {
+    [SerializeField] ParticleSystem _explosion;
+    [SerializeField] AudioSource _soundFx;
     ThrowableWeaponSource _weaponSource;
+    bool _isFirstCollision = true;
 
     void OnCollisionEnter(Collision other)
     {
-        if (_weaponSource.IsOpponentHit(other.collider))
+        if (_weaponSource.IsOpponentHit(other.collider) && _isFirstCollision)
         {
+            _isFirstCollision = false;
             Explode();
             _weaponSource.ApplyDamage(other.collider);
         }
@@ -15,8 +19,11 @@ public class ThrowableWeapon : MonoBehaviour
 
     void Explode()
     {
-        _weaponSource.PlayHitFx();
-        Destroy(gameObject);
+        GetComponent<MeshRenderer>().enabled = false;
+        _soundFx.Play();
+        _explosion.Play();
+        Destroy(gameObject, _explosion.main.duration);
+        //_weaponSource.PlayHitFx();
     }
 
     public void AssignWeaponSourceInstance(ThrowableWeaponSource src)
