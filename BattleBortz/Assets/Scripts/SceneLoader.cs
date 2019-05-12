d6b7bd499069d7f264010111eccf6372507ecbb8
argutierrez <argutierrez92@gmail.com>
2019-05-11 18:39:18 -0700

    Added escape button to quit

--- BattleBortz/Assets/Scripts/SceneLoader.cs
+++ BattleBortz/Assets/Scripts/SceneLoader.cs
@@ -3,6 +3,12 @@ using UnityEngine;
 
 public class SceneLoader : MonoBehaviour
 {
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();        
+    }
+
     public void LoadScene(int sceneNumber)
     {
         SceneManager.LoadScene(sceneNumber);
